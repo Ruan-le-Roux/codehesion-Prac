2e94b47c1823c9536937c427d68924eb15ab1059
Grad <grad@Grads-MBP.localdomain>
2025-09-15 11:07:52 +0200

    finished login working on register

---
+++ src/Login.jsx
@@ -0,0 +1,106 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import { useFormik, Form, Field } from 'formik'
+import {useNavigate} from 'react-router-dom'
+import './App.css'
+import axios from 'axios'
+// import { formData } from "new FormData"
+
+function Login() {
+  const [clientId, setClientId] = useState("");
+  const [clientSecret, setClientSecret] = useState("");
+  const [scope, setScope] = useState("");
+  // const [username, setUsername] = useState("");
+  // const [password, setPassword] = useState("");
+  const [isError, setError] = useState(false);
+
+  const navigate = useNavigate();
+  
+
+
+  const formik = useFormik({
+    initialValues: {
+      username: '',
+      password: '',
+    },
+    onSubmit: values => {
+      const formData = new URLSearchParams();
+      formData.append("grant_type", "password");
+      formData.append("client_id", "web-dashboard");
+      formData.append("client_secret", "SuperSecretPassword");
+      formData.append("scope", "openid profile role email offline_access adminApi mobileApi");
+      formData.append("username", values.username);
+      formData.append("password", values.password);
+
+      // console.log("here is the form data: ");
+      // for(let [key, value] of formData.entries())
+      // {
+      //   console.log(`${key}: ${value}`);
+      // }
+      // const formData = new FormData();
+      // formData.append("grant_type", "password");
+      // formData.append("client_id", "web-dashboard");
+      // formData.append("client_secret", "SuperSecretPassword");
+      // formData.append("scope", "openid profile role email offline_access adminApi mobileApi");
+      // formData.append("username", values.username);
+      // formData.append("password", values.password);
+
+      // console.log("here is the form data: ");
+      // for(let [key, value] of formData.entries())
+      // {
+      //   console.log(`${key}: ${value}`);
+      // }
+
+      axios.post('https://edeaf-api-staging.azurewebsites.net/connect/token', formData)
+      .then(function (response){
+        // console.log(response);
+        localStorage.setItem("access_token", response.data.access_token);
+        navigate('/register');
+      })
+      .catch(function (error){
+        localStorage.setItem("access_token","");
+        console.log(error);
+      })
+    }
+
+  });
+
+
+
+  return (
+    <div>
+      {/* <Formik
+        initialValues={{username: '', password: ''}}
+        // validate={values => {
+        //   const errors = {};
+        //   if(!values.username)
+        //   {
+        //     errors.email = 'Required';
+        //   }
+        // }}
+        // onSubmit= (values, {setSubmitting, resetForm}) => {
+
+        
+      > */}
+        <form onSubmit={formik.handleSubmit}>
+          <label htmlFor="username">UserName:</label>
+          <input type="email" id="username" name="username" placeholder="Username"  value={formik.values.username} onChange={formik.handleChange}/>
+
+          <br/>
+          
+          <label htmlFor="passwors">Password:</label>
+          <input type="password" id="password" name="password" placeholder="Password" value={formik.values.password} onChange={formik.handleChange}/>
+
+          <br/>
+          
+          <button type="submit">Sign In</button>
+        </form>
+
+      {/* </Formik> */}
+
+    </div>
+  )
+}
+
+export default Login;
---
+++ src/Register.jsx
@@ -0,0 +1,75 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import { useFormik, Form, Field } from 'formik'
+import './App.css'
+import axios from 'axios'
+// import { formData } from "new FormData"
+
+function Register() {
+    const access_token = localStorage.getItem("access_token");
+    // console.log(access_token);
+
+
+
+  const formik = useFormik({
+    initialValues: {
+      name: '',
+      surname: '',
+      email: '',
+    },
+    onSubmit: values => {
+      axios.post('https://edeaf-api-staging.azurewebsites.net/v1/admin/Users', {
+        "name": values.name,
+        "surname": values.surname,
+        "email": values.email,
+        "role": "Administrator",
+      },
+      {
+        headers:
+        {
+            "Authorization": `Bearer ${access_token}`,
+            "Content-Type": "application/json",
+        }
+      })
+      .then(function (response){
+        console.log(response);
+        // localStorage.setItem("access_token", response.data.access_token);
+      })
+      .catch(function (error){
+        console.log(error);
+      })
+    }
+
+  });
+
+
+
+  return (
+    <div>
+        <form onSubmit={formik.handleSubmit}>
+          <label htmlFor="name">Name:</label>
+          <input type="text" id="name" name="name" placeholder="Name"  value={formik.values.name} onChange={formik.handleChange}/>
+          
+          <br/>
+          
+          <label htmlFor="surname">Surname:</label>
+          <input type="text" id="surname" name="surname" placeholder="Surname"  value={formik.values.surname} onChange={formik.handleChange}/>
+          
+          <br/>
+
+
+          <label htmlFor="email">Email:</label>
+          <input type="email" id="email" name="email" placeholder="email@example.com"  value={formik.values.email} onChange={formik.handleChange}/>
+
+          {/* <br/> */}
+          <br/>
+          
+          <button type="submit">Sign In</button>
+        </form>
+
+    </div>
+  )
+}
+
+export default Register;
