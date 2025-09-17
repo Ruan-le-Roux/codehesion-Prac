7de48075c23f163c124d254880e21d4f7d53f8a4
Ruan le Roux <puan35.leroux@gmail.com>
2025-09-17 10:41:37 +0200

    words & categories done

--- src/AddToCategory.jsx
+++ src/AddToCategory.jsx
@@ -3,9 +3,11 @@ import {useState, useEffect} from 'react';
 import {useParams, useNavigate} from 'react-router-dom';
 import axios from 'axios';
 
+import styles from './CSS/AddToCategory.module.css';
+
 function AddToCategory()
 {
-    const [word, setWord] = useState([]);
+    const [word, setWord] = useState({});
     const {wordId} = useParams();
     const [categories, setCategories] = useState([]);
     const access_token = sessionStorage.getItem('access_token');
@@ -47,64 +49,81 @@ function AddToCategory()
     const formik = useFormik({
         initialValues: {
             name: word.name,
-            categories: []
+            formCategories: word.categories ? word.categories.map((c) => String(c.id)) : [],
         },
         enableReinitialize: true,
         onSubmit: values => {
-            categories.forEach(cat => {
-                
-                
-                    axios.put(`https://edeaf-api-staging.azurewebsites.net/v1/admin/categories/${cat.id}/words`, {
-                        "word": word.id,
-                    },
+            for(let cat of values.formCategories)
+            {
+                console.log(cat);
+                axios.patch(`https://edeaf-api-staging.azurewebsites.net/v1/admin/categories/${cat}/words`, {
+                    "wordId": word.id,
+                },
+                {
+                    headers:
                     {
-                        headers:
-                        {
-                            "Authorization": `Bearer ${access_token}`,
-                        }
-                    })
-                    .then(function (response){
-                        setAlert(true);
-                        setUpdated(true);
-                        navigate('/category', {state: {alert: true, message: "Word added to category successfully!", isGood: "success"}});
-                    })
-                    .catch(function (error){
-                        if(error.response && error.response.status === 404)
-                        {
-                            setError(`User does not exits. Double check all values`);
-                        }
-                        else
-                        {
-                            setError("Internal server error try again later.");
-                        }
-                        // console.log(error);
-                        // setError("There was an error loggin you in: ", error);
-                    })
-                
-            })
+                        "Authorization": `Bearer ${access_token}`,
+                    }
+                })
+                .then(function (response){
+                    navigate('/category', {state: {alert: true, message: "Categories ADDED successfully!", isGood: "success"}});
+                    // setAlert(true);
+                    // setUpdated(true);
+                })
+                .catch(function (error){
+                    if(error.response && error.response.status === 404)
+                    {
+                        // setError(`User does not exits. Double check all values`);
+                    }
+                    else
+                    {
+                        // setError("Internal server error try again later.");
+                    }
+                    // console.log(error);
+                    // setError("There was an error loggin you in: ", error);
+                })
+            }
         }
     });
 
+    const handleCategoryChange = (e) => {
+        const value = e.target.value;
+        if (formik.values.formCategories.includes(value)) 
+        {
+            formik.setFieldValue(
+                "formCategories",
+                formik.values.formCategories.filter((id) => id !== value)
+        );
+        } 
+        else 
+        {
+            formik.setFieldValue("formCategories", [
+                ...formik.values.formCategories,
+                value,
+            ]);
+        }
+    };
+
     return(
-        <div>
-            <form onSubmit={formik.handleSubmit}>
-                <p>Word: {word.name}</p>
+        <div className={styles.parent}>
+            <h1 className={styles.heading}>Add word to categories</h1>
+            <form onSubmit={formik.handleSubmit} className={styles.form}>
                 
-                <h1>Categories:</h1>
+                <h2 >Select Categories:</h2>
+                <h3>Word: {word.name}</h3>
+
                 {categories.map((cat, index) => (
-                    <div key={cat.id}>
-                        <label htmlFor="category">{cat.name}:</label>            
-                        <input type="checkbox" id="word" name="word" value={cat.name} onChange={formik.handleChange}/>
+                    <div key={cat.id} className={styles.categoryParent}>
+                        <label htmlFor={`category-${cat.id}`}>{cat.name}:</label>            
+                        <input type="checkbox" id={`category-${cat.id}`} name="category" value={String(cat.id)} onChange={handleCategoryChange} checked={formik.values.formCategories.includes(String(cat.id))}/>
                     </div>                    
                 ))}
 
-                <button type="submit">Add to categories</button>
-
-
+                <button type="submit" className={styles.button}>Add to categories</button>
+                <button  type="button" className={styles.button2} onClick={() => navigate(-1)}>Cancel</button>
             </form>
         </div>
     );
-
 }
 
 export default AddToCategory;
--- src/AllWords.jsx
+++ src/AllWords.jsx
@@ -1,6 +1,7 @@
 import {useState, useEffect} from 'react';
 import axios from 'axios';
-import {useNavigate} from 'react-router-dom';
+import {useNavigate, useLocation} from 'react-router-dom';
+import Alert from './Alert';
 
 import styles from './CSS/AllWords.module.css'
 
@@ -11,6 +12,10 @@ function AddToCategory()
     const [search, setSearch] = useState("");
     const access_token = sessionStorage.getItem("access_token");
     const navigate = useNavigate();
+    const location = useLocation(); 
+    const [alert, setAlert] = useState(false);
+    const [message, setMessage] = useState("");
+    const [isGood, setIsGood] = useState("");
 
     useEffect(() => {
         axios.get('https://edeaf-api-staging.azurewebsites.net/v1/admin/Words?PageSize=1000', {
@@ -41,22 +46,37 @@ function AddToCategory()
         }
 
     }, [search, words]);
-    return(
-        <div>
-            <div>
-                <input type="text" value={search} onChange={e => setSearch(e.target.value)}/>
 
+    useEffect(() => {
+        if(location.state?.alert)
+        {
+            setAlert(true);
+            setMessage(location.state.message);
+            setIsGood(location.state.isGood);
+            navigate(location.pathname, { replace: true });
+        }
+
+    }, [location.state]);
+
+    return(
+        <div className={styles.wordsParent}>
+            {alert && <Alert message={message} show={alert} type={isGood} onClose={() => {setAlert(false)}}/>}
+            
+            <div className={styles.searchParent}>
+                <input type="text" value={search} placeholder="Search words..." onChange={e => setSearch(e.target.value)}/>
             </div>
+
             <div className={styles.parent}>
                 {filteredWords.map((word) => (
-                    <div key={word.id}>
+                    <div key={word.id} className={styles.contentParent}>
                         <h1>{word.name}</h1>
                         <div className={styles.categoryParent}>
+                            {word.categories.length == 0 && <p className={styles.noCat}>No category</p>}
                             {word.categories.map((cat) => (
-                                <p key={cat.id}>{cat.name}</p>
+                                <p className={styles.cat} key={cat.id} >{cat.name}</p>
                             ))}
                         </div>
-                        <button type="button" onClick={() => navigate(`/category/${word.id}`)}>Add this word to a category</button>
+                        <button className={styles.button} type="button" onClick={() => navigate(`/category/${word.id}`)}>Add this word to a category</button>
                     </div>
                 ))}
             </div>
